a3dc50e5299ed72d7ba165f5f07bca11070e945d
tharanesh14 <tharanesh14@gmail.com>
2024-04-13 22:57:16 +0530

    new deploy 1

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
 # echo "BUILD START"
+python3 -m pip install Django
 python3 -m pip install -r requirements.txt
 # python3.9 manage.py collectstatic --noinput --clear
 python3 manage.py collectstatic
