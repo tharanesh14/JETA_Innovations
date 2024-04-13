ceeeb20c757baa146b3516532c118d962dadae26
tharanesh14 <tharanesh14@gmail.com>
2024-04-13 23:25:45 +0530

    new deploy 3

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,4 @@
-echo "BUILD START"
-# pip3 --version
 pip install -r requirements.txt
 # python3 pip install -r requirements.txt
 # python3.9 manage.py collectstatic --noinput --clear
 python3 manage.py collectstatic
-echo "BUILD END"
