dbaa8e4f115ecf4f2775acc06f738024d0269638
tharanesh14 <tharanesh14@gmail.com>
2024-04-20 23:08:18 +0530

    deploy update

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo "Building the project..."
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.10 get-pip.py
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic
+python3 get-pip.py
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic
