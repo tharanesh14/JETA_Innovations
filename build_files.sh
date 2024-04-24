8148429eb9f3273a03ea9132e79115aead222575
tharanesh14 <tharanesh14@gmail.com>
2024-04-24 12:50:51 +0530

    deploy update

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,6 @@
 echo "Building the project..."
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3.9 get-pip.py
-echo "Building the cmake..."
-python3.9 -m pip uninstall cmake
-python3.9 -m pip uninstall dlib
-python3.9 -m pip install cmake
-python3.9 -m pip install dlib
-echo "Building the cmake..."
 python3.9 -m cmake --version
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic
--- quat/wsgi.py
+++ quat/wsgi.py
@@ -8,11 +8,15 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quat.settings')
 
 application = get_wsgi_application()
+
+python_version = sys.version
+print("Python Runtime Version:", python_version)
 
 app = application
