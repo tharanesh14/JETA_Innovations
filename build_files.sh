bd780a9a8d947d23e8bc6db64206132643712f2f
tharanesh14 <tharanesh14@gmail.com>
2024-04-24 12:40:16 +0530

    deploy update

--- build_files.sh
+++ build_files.sh
@@ -3,6 +3,7 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3.9 get-pip.py
 echo "Building the cmake..."
 python3.9 -m pip install cmake
+python3.9 -m pip install dlib
 echo "Building the cmake..."
 python3.9 -m cmake --version
 python3.9 -m pip install -r requirements.txt
