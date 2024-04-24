4b773d6d14106313de4530f4d4027f32dda6eed1
tharanesh14 <tharanesh14@gmail.com>
2024-04-24 12:43:51 +0530

    deploy update

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,8 @@ echo "Building the project..."
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3.9 get-pip.py
 echo "Building the cmake..."
+python3.9 -m pip uninstall cmake
+python3.9 -m pip uninstall dlib
 python3.9 -m pip install cmake
 python3.9 -m pip install dlib
 echo "Building the cmake..."
