8f7378f33380117393932f46439e3501c57b6028
tharanesh14 <tharanesh14@gmail.com>
2024-04-25 16:32:53 +0530

    added

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,15 @@
 echo "Building the project..."
-python3.9 -c 'import sys; print("Python Runtime Version:", sys.version)'
+# python3.9 -c 'import sys; print("Python Runtime Version:", sys.version)'
 # Download and install pip
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.9 get-pip.py
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# python3.9 get-pip.py
 
 # Install project dependencies
 python3.9 pip install -r requirements.txt
 
 # Print Python runtime version
-python_version=$(python3 -c 'import sys; print(sys.version)')
-echo "Python Runtime Version: $python_version"
+# python_version=$(python3 -c 'import sys; print(sys.version)')
+# echo "Python Runtime Version: $python_version"
 
 # Run Django collectstatic command or any other build commands
 python3.9 manage.py collectstatic
